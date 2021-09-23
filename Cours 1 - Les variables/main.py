8bec89674080d8664a934a13d7e72c2d3fe0bc1f
souha <souhaiyle.daoudi.pro@gmail.com>
2021-09-23 21:39:26 +0200

    v1

---
+++ Cours 1 - Les variables/main.py
@@ -0,0 +1,12 @@
+from math import *
+from puissance import *
+
+
+print(sqrt(4))
+
+# Contenu du fichier main.py (programme principal)
+nombre = 5
+nombre_carre = carre(nombre)
+print("le carre vaut", nombre_carre)
+nombre_cube = cube(nombre)
+print("le cube vaut", nombre_cube)
