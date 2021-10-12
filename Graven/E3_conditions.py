0f1710b0a3971a327d07addb7a71e152f67119ee
souha <souhaiyle.daoudi.pro@gmail.com>
2021-10-12 22:32:55 +0200

    v1

--- Graven/E3_conditions.py
+++ Graven/E3_conditions.py
@@ -1,21 +1,12 @@
 # code
-#wallet = 5000
-computer_price = 900
+wallet = 5000
+computer_price = 1200
 
 
 # Le prix du pc est inférieur a 1000 euros
-#print(computer_price < 1000)
-
-def budget_pc():
-    print("prix du pc: ", computer_price )
-    budget = int(input("Quel est votre budget ?: "))
-    if budget >= computer_price:
-        print("Vous pouvez acheter le pc")
-    elif a ==
-
-    else:
-        print("Vous ne pouvez pas acheter le pc")
+if computer_price < 1000 :
+    print ("le prix de l'ordinateur est bien inferieur a 1000 euros")
+else :
+    print("le prix est supérieur")
 
 
-
-budget_pc()
