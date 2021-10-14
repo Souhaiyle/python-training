539769ee73c21fd6320f3a8eac1d9f7f1d3b224b
souha <souhaiyle.daoudi.pro@gmail.com>
2021-10-14 22:21:46 +0200

    v1

--- Exercices - Yassine/Exo 1 - boucles.py
+++ Exercices - Yassine/Exo 1 - boucles.py
@@ -1,9 +1,9 @@
 # Exo - 1 afficher à l'aide d'une boucle while ou for les éléments des listes animaux et nombres
 animaux=['girafe','tigre','lion','zebre','singe','poisson','fourmis','escargot','libellule']
 nombres =[1,2,3,4,5,6,8,9,10]
-
+# ici on crée une boucle for
 for i in animaux :
     print(i)
-
+# ici on crée une boucle for
 for i in nombres :
     print(i)
