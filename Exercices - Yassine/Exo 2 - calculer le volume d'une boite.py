197776ef59999251eb0ad9a72e354f1ab0c5eb9d
souha <souhaiyle.daoudi.pro@gmail.com>
2021-10-16 17:27:14 +0200

    v1

--- Exercices - Yassine/Exo 2 - calculer le volume d'une boite.py
+++ Exercices - Yassine/Exo 2 - calculer le volume d'une boite.py
@@ -1,16 +1,14 @@
 # 2 : créer une fonction volume_boite() qui prend en parametre la longueur l la largeur larg et la hauteur h
 # Puis determiner le volume d'une boite à partir des variables suivantes
 
-#exemple d'une fonction nom_de_la_fonction() qui prend en parametre une variable
+#ici on crée un foction volume_boite() et on determine le volume grace au variables l, h, L
 
-def nom_de_la_fonction (parametres):
-    print(parametres)
+#creation de la fonction
 
-def volume_boite (volume_boite):
-    hauteur = 5
-    longeur = 7
-    largeur= 6
-print(volume_boite)
+def volume_boite(longueur, largeur, hauteur):
+    volume = (longueur * largeur) / hauteur
+    volume_boite(2, 3, 5)
+    print(volume)
 
 
 
