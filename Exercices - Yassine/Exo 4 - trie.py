3506ed27c50f2f31ff155e298f838b8d29fc6237
souha <souhaiyle.daoudi.pro@gmail.com>
2021-10-17 16:34:56 +0200

    v1

--- Exercices - Yassine/Exo 4 - trie.py
+++ Exercices - Yassine/Exo 4 - trie.py
@@ -1,4 +1,5 @@
 # trier par ordre alphabetique la liste aniamux suivante
 
 animaux=['girafe','tigre','lion','zebre','singe','poisson','fourmis','escargot','libellule']
-
+sorted_list = sorted(animaux)
+print(sorted_list)
