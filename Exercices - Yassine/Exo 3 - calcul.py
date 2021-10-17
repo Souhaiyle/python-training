dfcdef549054674b4d730cd9240bb8137f4455e1
souha <souhaiyle.daoudi.pro@gmail.com>
2021-10-17 16:01:06 +0200

    v1

--- Exercices - Yassine/Exo 3 - calcul.py
+++ Exercices - Yassine/Exo 3 - calcul.py
@@ -1,14 +1,21 @@
 #Mettre n+4 à la puissance 2 et sauvegarder le résultat dans a. Ensuite, afficher a
-
 def n_plus_4_puisance_2(n):
-    print("resulat")
+    resultat = (n + 4) ** 2
+    print(resultat)
 
 
+n_plus_4_puisance_2(2)
 
 
 #Ajouter pi à n et sauvegarder le résultat dans a. Ensuite, afficher a
-def ajout_pi_a_n() :
-    print("resulat")
+def ajout_pi_a_n(n):
+    pi = 3.13
+    resultat = n + pi
+    print(resultat)
+
+
+ajout_pi_a_n(36)
+
 
 
 
