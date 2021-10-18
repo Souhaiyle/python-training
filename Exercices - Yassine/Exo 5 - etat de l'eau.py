0d55c34effcb75ebb00b5367b3498e07bdc8fea8
souha <souhaiyle.daoudi.pro@gmail.com>
2021-10-18 19:38:52 +0200

    v1

--- Exercices - Yassine/Exo 5 - etat de l'eau.py
+++ Exercices - Yassine/Exo 5 - etat de l'eau.py
@@ -5,3 +5,19 @@
 #Si la température est strictement négative alors l'eau est à l'état solide.
 #Si la température est entre 0 et 100 (compris) l'eau est à l'état liquide.
 #Si la température est strictement supérieure à 100.
+
+def temp(t):
+    t <= 0 and t >= 101
+    if t < 0:
+        print("L'état de l'eau est Solide")
+
+    elif t > 100:
+        print("L'état de l'eau est Gazeux")
+
+    else:
+        print("L'état de l'eau est Liquide")
+
+
+
+temp(50)
+
