da31b09a2da33a854efcfb1eabc1598711b57db3
souha <souhaiyle.daoudi.pro@gmail.com>
2021-10-18 23:11:00 +0200

    v1

--- Exercices - Yassine/Exos type controle.py
+++ Exercices - Yassine/Exos type controle.py
@@ -1,12 +1,25 @@
 def number(nb):
-    valeur_utilisateur = input("Entrez un nombre :")
+    nb = int(input("Entrez un nombre :"))
     if nb < 10 :
         print("Le nombre est pas compris entre 10 et 100")
     elif nb > 100:
         print("Le nombre est pas compris entre 10 et 100")
     else:
-        10<nb<100
         print("Le nombre est compris entre 10 et 100")
 
+    nb = nb%2
+    if nb == 0:
+        print("Le nombre est pair")
+    elif nb == 1:
+        print("Le nombre est impair")
 
-number(1000000)
+    if nb == 0 :
+        print(bin(nb))
+    elif nb == 1 :
+        print(hex(nb))
+
+number(100)
+
+
+
+
