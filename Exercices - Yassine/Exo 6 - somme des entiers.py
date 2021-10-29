4b0b51cdfa2458c0b8894abd43929b766cd43e31
souha <souhaiyle.daoudi.pro@gmail.com>
2021-10-29 20:01:35 +0200

    v1

--- Exercices - Yassine/Exo 6 - somme des entiers.py
+++ Exercices - Yassine/Exo 6 - somme des entiers.py
@@ -1,6 +1,7 @@
 #ecrire un programme pour qui affiche la somme des entiers de 1 à 10 c'est à dire 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10.
 #aide : utiliser une boucle
 
+
 s = 0
 for i in range(1,11):
     s = i + s
