360d7e1845f9814fe1b3f24bcea87ddb3dff1517
souha <souhaiyle.daoudi.pro@gmail.com>
2021-12-12 16:03:43 +0100

    v1

--- Exercices - Yassine/Jeu entrainement.py
+++ Exercices - Yassine/Jeu entrainement.py
@@ -1,6 +1,21 @@
-while nb >= 1 & nb <= 100:
-    input("Entrer un nombre : ")
-    if nb == 10:
-        print("Bravo, vous avez trouver le nombre ")
-    else:
-        print("Faux, ce n'est pas le bon nombre")
+from random import *
+nombre_machine = randint(0, 100)
+
+def devinne_mon_nombre(i):
+    while (0,100):
+        nombre_utilisateur = int(input("Entrer un nombre : "))
+        if nombre_utilisateur > nombre_machine:
+                print("Moins")
+        elif nombre_utilisateur == nombre_machine:
+            print("Bravo, vous avez trouver le nombre")
+        else:
+            print("Plus")
+
+
+devinne_mon_nombre(nombre_machine)
+
+
+
+
+
+
